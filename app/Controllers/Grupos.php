2d18cb7383b03825422ba0e7b2c4586c96164972
Plínio Hávila <76018725+pliniohavila@users.noreply.github.com>
2024-12-07 20:54:24 -0300

    80.11

--- app/Controllers/Grupos.php
+++ app/Controllers/Grupos.php
@@ -30,24 +30,72 @@ class Grupos extends BaseController
     public function exibir(int $id): string
     {
         $grupo = $this->buscaGrupoOu404($id);
-    
+
         $title = "Detalhes do Usuário: $grupo->nome";
         $data = ['titulo' => $title, 'grupo' => $grupo];
 
         return view('Grupos/exibir', $data);
     }
 
-    public function editar(int $id): string|RedirectResponse 
+    public function criar(): string|RedirectResponse
+    {
+        $grupo = new Grupo();
+
+        $title = 'Criando novo grupo acesso';
+        $data = ['titulo' => $title, 'grupo' => $grupo];
+
+        return view('Grupos/criar', $data);
+    }
+
+    public function editar(int $id): string|RedirectResponse
     {
         $grupo = $this->buscaGrupoOu404($id);
 
         if ($grupo->id == 1)
             return redirect()->back()->with('atencao', 'Não é possível editar o grupo:' . $grupo->nome);
-    
+
         $title = "Edição do Grupo: $grupo->nome";
         $data = ['titulo' => $title, 'grupo' => $grupo];
 
         return view('Grupos/editar', $data);
+    }
+
+    public function excluir(int $id)
+    {
+        $grupo = $this->buscaGrupoOu404($id);
+
+        if ($this->request->getMethod() === 'post') {
+            $this->grupoModel->delete($grupo->id);
+            return redirect()->to(site_url('grupos'))->with('sucesso', "Grupo $grupo->nome excluído com sucesso.");
+        }
+
+        $title = "Excluindo o Grupo: $grupo->nome";
+        $data = ['titulo' => $title, 'grupo' => $grupo];
+
+        return view('grupos/excluir', $data);
+    }
+
+    public function cadastrar(): ResponseInterface
+    {
+        if (!$this->request->isAJAX())
+            return redirect()->back();
+
+        $response['token'] = csrf_hash();
+
+        $post = $this->request->getPost();
+        $grupo = new Grupo($post);
+
+        if ($this->grupoModel->save($grupo)) {
+            $btnCriar = anchor('grupos/criar', 'Cadastrar novo grupo de acesso', ['class' => 'btn btn-danger mt-2']);
+            session()->setFlashdata('sucesso', "Dados salvos com sucesso! $btnCriar");
+            $response['id'] = $this->grupoModel->getInsertID();
+            return $this->response->setJSON($response);
+        }
+
+        $response['erro'] = 'Por favor, verifique os erros abaixo e tente novamente';
+        $response['erros_model'] = $this->grupoModel->errors();
+
+        return $this->response->setJSON($response);
     }
 
     public function recuperaGrupos(): ResponseInterface
@@ -55,16 +103,16 @@ class Grupos extends BaseController
         if (!$this->request->isAJAX())
             return redirect()->back();
         $attr = ['id', 'nome', 'descricao', 'exibir'];
-        $grupos = $this->grupoModel->select($attr)->orderBy('nome', 'ASC')->findAll();  
-        
+        $grupos = $this->grupoModel->select($attr)->orderBy('nome', 'ASC')->findAll();
+
         $data = [];
 
-        foreach($grupos as $grupo) {
+        foreach ($grupos as $grupo) {
             $nomeGrupo = esc($grupo->nome);
 
             $data[] = [
                 'nome' => anchor("/grupos/exibir/$grupo->id", $nomeGrupo, "title='Exibir $nomeGrupo'"),
-                'descricao' => esc($grupo->descricao), 
+                'descricao' => esc($grupo->descricao),
                 'exibir' => ($grupo->exibir) ? 'Sim' : 'Não'
             ];
         }
@@ -72,6 +120,42 @@ class Grupos extends BaseController
         $response = ['data' => $data];
         return $this->response->setJSON($response);
     }
+
+    public function atualizar(): ResponseInterface
+    {
+        if (!$this->request->isAJAX())
+            return redirect()->back();
+
+        $response['token'] = csrf_hash();
+
+        $post = $this->request->getPost();
+
+        $grupo = $this->buscaGrupoOu404($post['id']);
+
+        if ($grupo->id == 1) {
+            $response['erro'] = 'Por favor, verifique os erros abaixo e tente novamente';
+            $response['erros_model'] = ['grupo' => 'Não é possível editar o grupo:' . $grupo->nome];
+            return $this->response->setJSON($response);
+        }
+
+        $grupo->fill($post);
+        if (!$grupo->hasChanged()) {
+            $response['info'] = 'Não há novos dados para serem atualizados!';
+            return $this->response->setJSON($response);
+        }
+
+        if ($this->grupoModel->protect(false)->save($grupo)) {
+            session()->setFlashdata('sucesso', 'Grupo atualizado com sucesso!');
+            return $this->response->setJSON($response);
+        }
+
+
+        $response['erro'] = 'Por favor, verifique os erros abaixo e tente novamente';
+        $response['erros_model'] = $this->grupoModel->errors();
+
+        return $this->response->setJSON($response);
+    }
+
     private function buscaGrupoOu404(int $id = null)
     {
         if (!$id || !$grupo = $this->grupoModel->withDeleted(true)->find($id))
---
+++ app/Views/Grupos/excluir.php
@@ -0,0 +1,92 @@
+<?= $this->extend('Layout/principal') ?>
+
+<?= $this->section('titulo') ?>
+<?php echo $titulo; ?>
+<?= $this->endSection() ?>
+
+
+<?= $this->section('estilos') ?>
+<!-- Aqui será colocado estilos especificos -->
+<?= $this->endSection() ?>
+
+
+<?= $this->section('conteudo') ?>
+<div class="row">
+  <div class="col-6">
+    <div class="block">
+      
+      <div class="block-body">
+
+        <?php echo form_open_multipart('/grupos/excluir/' . $grupo->id); ?>
+
+        <div class="alert alert-warning" role="alert">
+          Tem deseja que excluir o usuário: <strong><?php echo $grupo->nome ?></strong> ?
+        </div>
+
+        <input type="submit" value="Excluir" id="btn-danger" class="btn btn-danger w-100 mt-2" />
+        <a href="<?php echo site_url("grupos/exibir/$grupo->id"); ?>" class="btn btn-info w-100 mt-2">Cancelar</a>
+        </div>
+
+        <?php echo form_close(); ?>
+      </div>
+    
+    </div> <!-- end div block -->
+  </div>
+</div>
+<?= $this->endSection() ?>
+
+
+<?= $this->section('scripts') ?>
+<!-- Aqui será colocado scripts especificos -->
+<script>
+  $(document).ready(function() {
+    $('#form').on('submit', function(e) {
+      e.preventDefault()
+      
+      $.ajax({
+        type: 'POST', 
+        url: '<?php echo site_url('grupos/upload'); ?>', 
+        data: new FormData(this), 
+        dataType: 'json', 
+        contentType: false, 
+        cache: false, 
+        processData: false, 
+        beforeSend: function() {
+          $('#response').html('')
+          $('#btn-salvar').html('Aguarde...')
+          $('#btn-salvar').html('Aguarde...')
+        }, 
+        success: function(response) {
+          $('#btn-salvar').html('Salvar')
+          $('#btn-salvar').removeAttr('disabled')
+
+          $('[name=csrf_ordem]').val(response.token)
+          if (!response.erro && !response.erros_model)
+            window.location.href = `<?php echo site_url("grupos/exibir/$grupo->id"); ?>`
+
+          if (response.erro)
+            $('#response').html(`<div class="alert alert-danger">${response.erro}</div>`)
+            
+          if (response.erros_model) {
+              $.each(response.erros_model, function (key, value) {
+                // $('#response').append(`<ul class="list-unstyled"><li class="text-danger">value</li></ul>`)
+                $('#response').append(`<p class="alert-danger mt-0">${value}</p>`)
+              })
+            }
+        }, 
+        error: function() {
+          alert('Ocorreu um erro no back-end')
+          $('#btn-salvar').html('Salvar')
+          $('#btn-salvar').removeAttr('disabled')
+        }
+      })
+
+    })
+
+    // $("#form").submit(function() {
+    //   $(this).find(":submit").attr('disable', 'disabled')
+    // })
+  })
+
+</script>
+<?= $this->endSection() ?>
